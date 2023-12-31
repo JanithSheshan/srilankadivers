a619823878cb29225d2e2de9ebb689546b2c8f30
JaniShesh <jasheshan011@gmail.com>
2024-01-01 00:24:30 +0530

    contact form

--- forms/contact.php
+++ forms/contact.php
@@ -1,22 +1,16 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    
-    $to = 'jasheshan011@gmail.com'; // Replace with your email address
-    $headers = "From: $email\r\n";
-    $message = "Name: $name\nEmail: $email\nSubject: $subject\n\n$message";
-    
-    // Send the email
-    if (mail($to, $subject, $message, $headers)) {
-        echo "success"; // You can customize this response as needed
-    } else {
-        echo "error"; // You can customize this response as needed
-    }
-} else {
-    // Handle invalid requests
-    echo "Invalid request";
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+$to = "jasheshan011@gmail.com";
+$subject = "Mail From website :".$subject;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@srilankadivers.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
+//redirect
+header("Location:../index.html#contact");
 ?>
